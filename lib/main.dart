457084ce5df4c74e91ae7e3ecb7c6046d508552c
Srasti Verma <srasti.verma@coditas.com>
2022-08-09 14:54:41 +0530

    added counter

--- lib/main.dart
+++ lib/main.dart
@@ -19,48 +19,59 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: ChangeNotifierProvider<TaskProvider>(
-        create: (_) => TaskProvider(),
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            centerTitle: false,
-            backgroundColor: const Color.fromARGB(255, 0, 191, 252),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const <Widget>[
-                Expanded(
-                  flex: 3,
-                  child: Text('To Do List'),
+          create: (_) => TaskProvider(),
+          builder: (context, child) {
+            return Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+                centerTitle: false,
+                backgroundColor: const Color.fromARGB(255, 0, 191, 252),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: Text('To Do List'),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Text('Done:'),
+                          Text(
+                            Provider.of<TaskProvider>(context, listen: true)
+                                .count
+                                .toString(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Text('Done:2'),
-                ),
-              ],
-            ),
-          ),
-          body: ListView(
-            children: taskList.map(
-              (element) {
-                return Container(
-                  decoration: kListTileBorder,
-                  height: 71,
-                  child: GestureDetector(
-                    onTap: (() {
-                      if (isChecked = true) {
-                        const TextStyle(decoration: TextDecoration.lineThrough);
-                        //count++;
-                        //element.isTaskCompleted ? count++ : null;
-                      }
-                    }),
-                    child: ListTileElement(element),
-                  ),
-                );
-              },
-            ).toList(),
-          ),
-        ),
-      ),
+              ),
+              body: ListView(
+                children: taskList.map(
+                  (element) {
+                    return Container(
+                      decoration: kListTileBorder,
+                      height: 71,
+                      child: GestureDetector(
+                        onTap: (() {
+                          if (isChecked = true) {
+                            const TextStyle(
+                                decoration: TextDecoration.lineThrough);
+                            //count++;
+                            //element.isTaskCompleted ? count++ : null;
+                          }
+                        }),
+                        child: ListTileElement(element),
+                      ),
+                    );
+                  },
+                ).toList(),
+              ),
+            );
+          }),
     );
   }
 }
--- lib/provider/task_provider.dart
+++ lib/provider/task_provider.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 import '../todo_list_class.dart';
 
 class TaskProvider with ChangeNotifier {
-   int totalTaskDone = 0;
+  int totalTaskDone = 0;
   int tempElement = 0;
+  int count = 0;
   taskCompleted(TodoListElements element) {
     if (element.isTaskCompleted) {
       element.isTaskCompleted = false;
       totalTaskDone--;
+      count--;
     } else {
       element.isTaskCompleted = true;
-        totalTaskDone++;
+      totalTaskDone++;
+      count++;
     }
     notifyListeners();
   }
